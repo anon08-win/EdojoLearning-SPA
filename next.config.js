566e87dda62ef628b4bc70e7dfc733b16afe416a
Aishwary Pratap Singh <aishwarypratapsingh@Ashs-MacBook-Air.local>
2026-06-14 18:34:55 +0530

    Fix chromium deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['puppeteer', 'nodemailer'],
-  },
+  serverExternalPackages: [
+    '@sparticuz/chromium',
+    'puppeteer-core',
+  ],
 };
 
 module.exports = nextConfig;
